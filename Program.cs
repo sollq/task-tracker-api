2e160f08dd31aafd41012a07101dbadb9382a776
sollq <kirwhow@gmail.com>
2024-10-11 00:12:28 +0300

    Added Auto Migration (For work in container)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using TaskTrackerAPI.Data;
 using TaskTrackerAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,12 @@ builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.ConfigureMiddlewarePipeline();
 
